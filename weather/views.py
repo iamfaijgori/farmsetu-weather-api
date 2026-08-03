3231b0e7c1c64683f014cd67ef4e381220f8dad3
iamfgx <faijangori2004@outlook.com>
2026-08-03 20:03:35 +0530

    API integration and checking api endpoint

--- weather/views.py
+++ weather/views.py
@@ -5,3 +5,16 @@ from .serializers import WeatherRecordSerializer
 class WeatherRecordViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WeatherRecord.objects.all()
     serializer_class = WeatherRecordSerializer
+    
+    # Enable filtering directly from the URL query params
+    def get_queryset(self):
+        queryset = WeatherRecord.objects.all()
+        region = self.request.query_params.get('region')
+        year = self.request.query_params.get('year')
+        
+        if region:
+            queryset = queryset.filter(region__iexact=region)
+        if year:
+            queryset = queryset.filter(year=year)
+            
+        return queryset
