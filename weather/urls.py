1715385ccd9bd40fc119a3e3d632a59f15227672
iamfgx <faijangori2004@outlook.com>
2026-08-07 00:29:04 +0530

    Security patch: Remove temporary DB reset endpoint

--- weather/urls.py
+++ weather/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WeatherRecordViewSet, reset_cloud_database # <-- Add import here
+from .views import WeatherRecordViewSet
 
 router = DefaultRouter()
 router.register(r'weather', WeatherRecordViewSet, basename='weather')
 
 urlpatterns = [
-    # TEMPORARY HIDDEN ROUTE
-    path('db-reset-secure-endpoint/', reset_cloud_database, name='db-reset'),
     
     path('', include(router.urls)),
 ]
